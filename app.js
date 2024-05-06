e4ad0e119b0753a7ba7a6ccb54d7b195e23bb45c
Syeda Samra <159478028+Samra050@users.noreply.github.com>
2024-05-06 11:45:04 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+import { differenceInSeconds } from "date-fns";
+function* countdowntimer(second) {
+    while (second > 0) {
+        yield second;
+        second--;
+    }
+}
+let timerIterator = countdowntimer(10);
+function displaycountdown() {
+    let result = timerIterator.next();
+    if (!result.done) {
+        const now = new Date();
+        const countdowntime = new Date(now.getTime() + (result.value * 1000));
+        const remainingseconds = differenceInSeconds(countdowntime, now);
+        console.log(`Remaining Seconds:${remainingseconds}`);
+        setTimeout(displaycountdown, 1000);
+    }
+    else {
+        console.log("countdown complete!");
+    }
+}
+displaycountdown();
